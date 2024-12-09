6488ce73581861fb7cf1c8efc00b500d216f88e1
charm <ichchinmay.in@gmail.com>
2024-12-09 22:14:36 +0530

    logic fix, homepage data added,

--- src/data/quality.jsx
+++ src/data/quality.jsx
@@ -1,35 +1,41 @@
+import excellent from "../assets/images/excellent.png";
+import good from "../assets/images/good.png";
+import average from "../assets/images/average.png";
+import poor from "../assets/images/poor.png";
+import severe from "../assets/images/severe.png";
+
 const quality = [
     {
         id: 1,
-        imageLink: "src\assets\images\excellent.png",  // Replace with actual image URL
+        imageLink: excellent,  // Replace with actual image URL
         title: "Excellent",
         class: "Class A",
         subtitle: "Crystal clear and pure, ideal for drinking and bathing."
       },
       {
         id: 2,
-        imageLink: "src\assets\images\good.png",  // Replace with actual image URL
+        imageLink: good,  // Replace with actual image URL
         title: "Good",
         class: "Class B",
         subtitle: "Safe to use, requires minimal treatment."
       },
       {
         id: 3,
-        imageLink: "src\assets\images\average.png",  // Replace with actual image URL
+        imageLink: average,  // Replace with actual image URL
         title: "Average",
         class: "Class C",
         subtitle: "Acceptable quality, may need moderate treatment."
       },
       {
         id: 4,
-        imageLink: "src\assets\images\poor.png",  // Replace with actual image URL
+        imageLink: poor,  // Replace with actual image URL
         title: "Poor",
         class: "Class D",
         subtitle: "Poor quality, requires treatment before use for drinking or bathing."
       },
       {
         id: 5,
-        imageLink: "src\assets\images\severe.png",  // Replace with actual image URL
+        imageLink: severe,  // Replace with actual image URL
         title: "Severe",
         class: "Class E",
         subtitle: "Hazardous quality, unsuitable for use without extensive treatment."
